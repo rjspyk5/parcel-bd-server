51fe38d1dfe25cffcd25827f0ae39181de59eb56
unknown <rjspyk5@gmail.com>
2025-12-27 18:03:01 +0600

    chore:create routes of auth,user,parcel

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
-export const router = Router();
-router.use("/users", userRouter)
+import { authRouter } from "../modules/auth/auth.routes.js";
+import { userRouter } from "../modules/users/userRouter.js";
+export const routeEntry = Router();
+routeEntry.use("/auth", authRouter);
+routeEntry.use("/user", userRouter);
